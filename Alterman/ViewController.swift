d9409698eeac4ad918836783b6bb22128bcfa98f
sJonathan Ohayon <me@jonathano.com>
2017-07-07 18:19:50 +0300

    Initial Commit

---
+++ Alterman/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Alterman
+//
+//  Created by Jonathan Ohayon on 07/07/2017.
+//  Copyright © 2017 Jonathan Ohayon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
